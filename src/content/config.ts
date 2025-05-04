d7eb0368792087dc380e5caf88396982bace71f1
adamelkadaoui <adam.elkadaoui@gmail.com>
2025-05-05 00:13:36 +0200

    feat: add astro file for project generations pages and contents

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,5 @@
 import { z, defineCollection } from "astro:content";
+
 const blogSchema = z.object({
     title: z.string(),
     description: z.string(),
@@ -24,13 +25,27 @@ const storeSchema = z.object({
     heroImage: z.string().optional(),
 });
 
+const projectSchema = z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    stack: z.array(z.string()),
+    heroImage: z.string().optional(),
+    badge: z.string().optional(),
+    url: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+});
+
 export type BlogSchema = z.infer<typeof blogSchema>;
 export type StoreSchema = z.infer<typeof storeSchema>;
+export type ProjectSchema = z.infer<typeof projectSchema>;
 
 const blogCollection = defineCollection({ schema: blogSchema });
 const storeCollection = defineCollection({ schema: storeSchema });
+const projectCollection = defineCollection({ schema: projectSchema });
 
 export const collections = {
     'blog': blogCollection,
-    'store': storeCollection
-}
+    'store': storeCollection,
+    'project': projectCollection
+};
