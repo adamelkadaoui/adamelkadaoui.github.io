a087a498a5f25dccb00dd453470ec5099f3c3e26
adamelkadaoui <adam.elkadaoui@gmail.com>
2025-05-04 03:06:29 +0200

    feat: manage multilangage support

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,10 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adamelkadaoui.github.io/',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  i18n: {
+    locales: ["fr", "en"],
+    defaultLocale: "fr",
+    prefixDefaultLocale: true
+  }
 });
