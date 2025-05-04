4099c82874e918360ca42b5b6e9362affeac303c
adamelkadaoui <adam.elkadaoui@gmail.com>
2025-05-05 01:26:33 +0200

    feat: add site map integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://adamelkadaoui.github.io/',
+  site: 'https://adamelkadaoui.github.io',
   integrations: [mdx(), sitemap(), tailwind()],
 });
