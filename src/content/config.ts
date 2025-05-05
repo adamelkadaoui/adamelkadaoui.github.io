db04c9546575760f43e9be8bf678b93882160120
adamelkadaoui <adam.elkadaoui@gmail.com>
2025-05-05 02:35:19 +0200

    feat: improve optimisation and speed rendering

--- src/content/config.ts
+++ src/content/config.ts
@@ -33,7 +33,9 @@ const projectSchema = z.object({
     heroImage: z.string().optional(),
     badge: z.string().optional(),
     url: z.string().optional(),
-    tags: z.array(z.string()).optional(),
+    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
+      message: 'tags must be unique',
+  }).optional(),
 });
 
 export type BlogSchema = z.infer<typeof blogSchema>;
